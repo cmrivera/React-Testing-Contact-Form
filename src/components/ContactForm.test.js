30b3d06133fe1b7b7b76b3f97c01fa1b07a78623
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-11 17:34:55 -0400

    first test started, ok so far

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ContactForm from "./ContactForm.js";
+
+test("renders contact form without crashing", () => {
+  render(<ContactForm />);
+});
+
+test("");
